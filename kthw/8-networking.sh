670c8dd015a795ac7a52c443a5b704a1904bffe0
Moses Jilugu <mosesjil@ie.ibm.com>
2019-05-20 16:29:51 +0100

    Additional rough notes

--- kthw/8-networking.sh
+++ kthw/8-networking.sh
@@ -4,13 +4,20 @@
 #  https://kubernetes.io/docs/concepts/cluster-administration/networking/
 #  https://github.com/weaveworks/weave
 
-# Enable port forwarding on both worker nodes
+# Enable IP forwarding on both worker nodes
 
 sudo sysctl net.ipv4.conf.all.forwarding=1
 echo "net.ipv4.conf.all.forwarding=1" | sudo tee -a /etc/sysctl.conf
 
 # Install Weave Net
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')&env.IPALLOC_RANGE=10.200.0.0/16"
+
+# Verify that weave net pods are up and running
+kubectl get pods -n kube-system
+
+NAME              READY     STATUS    RESTARTS   AGE
+weave-net-m69xq   2/2       Running   0          11s
+weave-net-vmb2n   2/2       Running   0          11s
 
 # Create a Nginx deployment with 2 replicas:
 
@@ -49,18 +56,25 @@ POD_NAME=$(kubectl get pods -l run=busybox -o jsonpath="{.items[0].metadata.name
 
 kubectl get ep nginx
 
+NAME      ENDPOINTS                       AGE
+nginx     10.200.0.2:80,10.200.128.1:80   50m
+
 # Make sure the busybox pod can connect to nginx pod on both of those IP
 # addresses
-kubectl exec $POD_NAME -- curl <first nginx pod IP address>
-kubectl exec $POD_NAME -- curl <second nginx pod IP address>
+kubectl exec $POD_NAME -- curl 10.200.0.2:80
+kubectl exec $POD_NAME -- curl 10.200.128.1:80
 
 # Verify that we can connect to the services
 
 kubectl get svc
 
+NAME         TYPE        CLUSTER-IP   EXTERNAL-IP   PORT(S)   AGE
+kubernetes   ClusterIP   10.32.0.1    <none>        443/TCP   1h
+nginx        ClusterIP   10.32.0.54   <none>        80/TCP    53m
+
 # Make sure we can access services from the busybox pod
 
-kubectl exec $POD_NAME -- curl <nginx service IP address>
+kubectl exec $POD_NAME -- curl 10.32.0.54
 
 # Cleanup
 
---
+++ kthw/9-deploy-dns-cluster-addon.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+## Links
+#   https://kubernetes.io/docs/concepts/services-networking/dns-pod-service/
+
+# might need to do coredns instead of kube-dns here
+# https://kubernetes.io/docs/tasks/administer-cluster/coredns/
+
+###### Now obsolete ##########
+
+# installing kube-dns
+kubectl create -f https://storage.googleapis.com/kubernetes-the-hard-way/kube-dns.yaml
+
+# verify kube-dns pods start correctly
+kubectl get pods -l k8s-app=kube-dns -n kube-system
+
+NAME                        READY     STATUS    RESTARTS   AGE
+kube-dns-598d7bf7d4-spbmj   3/3       Running   0          36s
+
+# Test kube dns
+
+kubectl run busybox --image=busybox:1.28 --command -- sleep 3600
+POD_NAME=$(kubectl get pods -l run=busybox -o jsonpath="{.items[0].metadata.name}")
+
+kubectl exec -ti $POD_NAME -- nslookup kubernetes
+
+# Should get something like this
+Server:    10.32.0.10
+Address 1: 10.32.0.10 kube-dns.kube-system.svc.cluster.local
+
+Name:      kubernetes
+Address 1: 10.32.0.1 kubernetes.default.svc.cluster.local
+
+# Cleanup test objects
+
+kubectl delete deployment busybox
